e887827c17818f2050594d4d8a84d8c657591787
KangCode <kangtadragon@gmail.com>
2018-06-15 17:08:25 +0700

    Creating Block1

---
+++ templates/block1.php
@@ -0,0 +1,26 @@
+<div class="block-gioithieu">
+<div class="container nopadding">
+    <div class="col-sm-12 col-md-12 col-xs-12 logo">
+        <div class="div-of-logo">
+        <a href="#"><img src="images/logo.png" style="width:100%;"></a>
+        </div>
+    </div>
+    <div class="col-sm-12 col-md-12 col-xs-12 slogan">
+        <div class="div-of-slogan">
+        <a href="#"><img src="images/slogan.png" style="width:100%;"></a>
+        </div>
+    </div>
+    <div class="col-sm-12 col-md-12 col-xs-12 textblock1">
+        <div class="div-of-textblock1">
+        <p>Trong mỗi chúng ta đều có cho riêng mình một nơi để trở về sau những lo toan, bộn bề của cuộc sống tất bật thường ngày.</p>
+        <p>Nơi chúng ta được chào đòn bằng sự bình yên, nơi ta được bảo bọc trong sự ấm áp, nơi ta được vỗ về, động viên bằng sự yêu thương, nơi ta gọi là nhà.</p>
+        </div>
+    </div>
+    <div class="col-sm-12 col-md-12 col-xs-12 textblock2">
+        <div class="div-of-textblock1">
+        <p>Trong mỗi chúng ta đều có cho riêng mình một nơi để trở về sau những lo toan, bộn bề của cuộc sống tất bật thường ngày.</p>
+        <p>Nơi chúng ta được chào đòn bằng sự bình yên, nơi ta được bảo bọc trong sự ấm áp, nơi ta được vỗ về, động viên bằng sự yêu thương, nơi ta gọi là nhà.</p>
+        </div>
+    </div>
+</div>
+</div>
