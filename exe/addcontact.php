031d63de0f02bfbbd32eea0fc61413737ca1d934
Nhut Nguyen Minh <nhut.nguyen@salto.vn>
2020-07-11 13:01:25 +0700

    add contact fix

--- exe/addcontact.php
+++ exe/addcontact.php
@@ -6,12 +6,10 @@ if (isset($_POST['submit'])) {
   $name = $_POST['hoten'];
   $sdt = $_POST['sdt'];
   $email = $_POST['email'];
-  $ids = "SELECT COUNT(*) FROM data";
-  $dd = pg_query($conn, $ids);
-  $sql = "INSERT INTO data(id, name, phone, email) 
-              VALUES (" . $dd + 1 . ", $name','$sdt','$email')";
+  $sql = "INSERT INTO data(name, phone, email) 
+              VALUES ('$name', '$sdt', '$email')";
   pg_query($conn, $sql);
-  if ($conn->query($sql)) {
+  if (pg_query($conn, $sql)) {
     $_SESSION['flash'] = "<br><div class='alert alert-success text-center'>Thông tin đã được gửi đi</div>";
     header("Location:../index.php#footer");
   } else {
