ae8ae572309b7eb5149f83df122b883e4bf7e3a5
Nhut Nguyen Minh <nhut.nguyen@salto.vn>
2020-07-11 12:33:17 +0700

    add contact fix

--- exe/addcontact.php
+++ exe/addcontact.php
@@ -1,19 +1,19 @@
 <?php
-  ob_start();
-  session_start();
-  require_once('../config.php');
-  if(isset($_POST['submit'])){
-    $name = $_POST['hoten'];
-    $sdt = $_POST['sdt'];
-    $email = $_POST['email'];
-    $sql = "INSERT INTO data(name, phone, email) 
+ob_start();
+session_start();
+require_once('../config.php');
+if (isset($_POST['submit'])) {
+  $name = $_POST['hoten'];
+  $sdt = $_POST['sdt'];
+  $email = $_POST['email'];
+  $sql = "INSERT INTO data(name, phone, email) 
               VALUES ('$name','$sdt','$email')";
-    if($conn->query($sql)){
-        $_SESSION['flash'] = "<br><div class='alert alert-success text-center'>Thông tin đã được gửi đi</div>";
-        header("Location:../index.php#footer");
-    }
-    else{
-        $_SESSION['flash'] = "<br><div class='alert alert-danger text-center'>Có lỗi trong quá trình gửi đi</div>";
-        header("Location:../index.php#footer");
-    }
+  var_dump($sql);
+  if ($conn->query($sql)) {
+    $_SESSION['flash'] = "<br><div class='alert alert-success text-center'>Thông tin đã được gửi đi</div>";
+    header("Location:../index.php#footer");
+  } else {
+    $_SESSION['flash'] = "<br><div class='alert alert-danger text-center'>Có lỗi trong quá trình gửi đi</div>";
+    header("Location:../index.php#footer");
   }
+}
