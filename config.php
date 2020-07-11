1caefe0932436823a2cbc9e41dfabef0e2f4bef5
Nhut Nguyen Minh <nhut.nguyen@salto.vn>
2020-07-11 11:26:43 +0700

    change connect to PG
    change connect to PG

--- config.php
+++ config.php
@@ -1,17 +1,18 @@
 <?php
-$hostname = 'localhost';
-$username = 'root'; 
-$password = '';
-$dbname = 'chautho';
+$hostname = 'ec2-52-204-232-46.compute-1.amazonaws.com';
+$username = 'gbfamrjewhkjds';
+$password = '3e51abc23d1f2393ef589620980c95e94116b281980d85259ecdbc6b584f44ea';
+$dbname = 'df1g2isvvr82lq';
 define('HOSTNAME',$hostname);
 define('USERNAME',$username);
 define('PASSWORD',$password);
 define('DBNAME',$dbname);
-$conn = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DBNAME);
+//$conn = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DBNAME);
+$conn = pg_connect("host=ec2-52-204-232-46.compute-1.amazonaws.com port=5432 dbname=df1g2isvvr82lq user=gbfamrjewhkjds password=3e51abc23d1f2393ef589620980c95e94116b281980d85259ecdbc6b584f44ea");
 //require_once("../backup.php");
 // Kiểm tra kết nối cơ sở dữ liệu
 if (mysqli_connect_errno()){
   echo "Thất bại trong việc cơ sở dữ liệu " . mysqli_connect_error();
 }
-mysqli_set_charset($conn,"utf8");
+//mysqli_set_charset($conn,"utf8");
 ?>
